a80127f9cd193b576584f6e9f522bb17d070da93
Eric Thelin <extern.eric.thelin@digg.se>
2025-09-08 17:21:58 +0200

    feat: add script for repeating the same command until failure
    This is useful for checking the stability of test suites. For instance,
    to repeat the test suite of this project until failure, or max 100
    times, you can use the command below:
    
    	./development/until_failure.sh 'npm run test:once'
    
    Signed-off-by: Eric Thelin <extern.eric.thelin@digg.se>

---
+++ development/until_failure.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# This scripts provides a way to run the same command repeateadly until it
+# fails. It can be used to check the stability of test suites or individual
+# tests. By default the given command is repeated 100 times.
+#
+#	# Rerun 'npm run test:once' until it fails or maximum 100 times
+# 	./development/until_failure.sh 'npm run test:once'
+#
+#	# Same as above, but stop after ten iterations
+# 	./development/until_failure.sh 'npm run test:once' 10
+#
+readonly command="$1"
+readonly max="${2:-100}"
+
+echo "Running command '$command' $max times or until failure"
+for n in $(seq 1 "$max"); do
+  echo "Attempt $n of $max"
+
+  if $command; then
+    echo 'Success!'
+  else
+    echo "Failed on attempt $n!"
+    exit 1
+  fi
+done
+echo "Tried $max times without failure."
